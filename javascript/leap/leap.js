1c1282cf121c68d9ff981e7ce7e3b9c88b5bede1
João Vicente Souto <joaovicentesouto@gmail.com>
2017-01-28 17:09:16 -0200

    Primeiros exercícios através da plataforma do exercism.io

---
+++ javascript/leap/leap.js
@@ -0,0 +1,20 @@
+//
+// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+var Year = function(year) {
+    this.year = year;
+};
+
+Year.prototype.isLeap = function() {
+  if(this.year % 100 == 0) {
+      if(this.year % 400 == 0) {
+          return true;
+      }
+      return false
+  }
+  return (this.year % 4) == 0;
+};
+
+module.exports = Year;
