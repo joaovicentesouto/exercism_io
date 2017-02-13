65bbf82d38b65a0995b9673b29f14a592b4996ab
João Vicente Souto <joaovicentesouto@gmail.com>
2017-02-13 09:50:01 -0200

    Phone Number: completed

--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -1,28 +1,20 @@
 var PhoneNumber = function(digits) {
     digits = this.rulesTest(digits.replace(/\D/g , ''));
-    this.areaCodeOfNumber = this.subString(digits, 0, 2);
-    this.firstPieceOfNumber = this.subString(digits, 3, 5);
-    this.lastPieceOfNumber = this.subString(digits, 6, 9);
-};
-
-PhoneNumber.prototype.subString = function(string, firstIndex, lastIndex) {
-  subString = '';
-  for(firstIndex; firstIndex <= lastIndex; firstIndex++) {
-    subString += string[firstIndex];
-  }
-  return subString;
+    this.areaCodeOfNumber = digits.substr(0, 3);
+    this.firstPieceOfNumber = digits.substr(3, 3);
+    this.lastPieceOfNumber = digits.substr(6, 4);
 };
 
 PhoneNumber.prototype.rulesTest = function(digits) {
-    return (digits.length<10 || digits.length>11 || (digits.length==11 && digits[0]!=1))?
-           '0000000000' : digits.length==10 ? digits : this.subString(digits, 1, 10);
+  return digits.length<10 || digits.length>11 || (digits.length==11 && digits[0]!=1) ?
+           '0000000000' : digits.length==10 ? digits : digits.substr(1, 10);
 };
 
 PhoneNumber.prototype.number = function() {
-   return this.areaCodeOfNumber + this.firstPieceOfNumber + this.lastPieceOfNumber;
+  return this.areaCodeOfNumber + this.firstPieceOfNumber + this.lastPieceOfNumber;
 };
 
-PhoneNumber.prototype.areaCode = function(areaCode) {
+PhoneNumber.prototype.areaCode = function() {
   return this.areaCodeOfNumber;
 };
 
