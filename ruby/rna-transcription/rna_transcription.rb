a7f8e007b5596f75a3ec367d9bac723f9c3e0195
João Vicente Souto <joaovicentesouto@gmail.com>
2017-02-21 21:44:55 -0300

    JavaScript: -Food Chain: started. Ruby: -Rna Trans: completed.

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,7 +1,20 @@
 class Complement
 
+  @@transformation = {'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U'}
+
   def self.of_dna(strand)
-    'G'
+    rna_complement = ''
+    validStrand = true
+
+    strand.each_char { |nucleotide|
+      if(@@transformation.has_key?(nucleotide))
+        rna_complement += @@transformation[nucleotide]
+      else
+        validStrand = false
+      end
+    }
+
+    validStrand ? rna_complement : ''
   end
 
 end
