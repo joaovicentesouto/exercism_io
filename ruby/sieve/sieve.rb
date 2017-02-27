51de2d435e405182ffa468c349f13424343baf99
João Vicente Souto <joaovicentesouto@gmail.com>
2017-02-27 14:21:53 -0300

    Ruby: -Sieve: completed.

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -5,12 +5,31 @@ class Sieve
   end
 
   def primes
-    arrayOfPossibilities = Array.new(@number-1, true)
+    arrayOfPossibilities = Array.new(@number-1, true) #The array index is relative to index+2
+    primes = []
     arrayOfPossibilities.each_with_index { |condition, index|
       if condition
-        
+        findingMultiple(index+2, arrayOfPossibilities, @number)
+        primes.push(index+2)
       end
     }
+    primes
   end
 
+  def findingMultiple(base, arrayOfPossibilities, limit)
+    squareBase = base ** 2
+    for times in 0..limit
+      multiple = squareBase+(times*base)
+      if(multiple <= limit)
+        arrayOfPossibilities[multiple-2] = false
+      else
+        break
+      end
+    end
+  end
+
+end
+
+module BookKeeping
+  VERSION = 1
 end
